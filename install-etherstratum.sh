6b524acc1494f06d081bdacdd4f69543787d60a8
Mohammed Mahmoud <166178531+MOHAPY24@users.noreply.github.com>
2025-09-28 12:01:34 +0400

    Add files via upload

--- install-etherstratum.sh
+++ install-etherstratum.sh
@@ -40,14 +40,10 @@ sudo apt install software-properties-common
 
 
 udo apt install -y fastfetch || echo "Fastfetch install failed, check Debian source"
-
+sudo apt install fastfetch
 
 #curl -fsSL https://ollama.com/install.sh | sh
 #ollama pull tinyllama
-
-curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt install spotify-client
 
 wget https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.8_all.deb
 sudo dpkg -i ./protonvpn-stable-release_1.0.8_all.deb && sudo apt update
@@ -55,23 +51,32 @@ sudo apt install proton-vpn-gnome-desktop
 
 
 sudo apt install -y openvpn neofetch samba 
-sudo apt install -y bspwm
-sudo apt install -y openvpn vim emacs nano 
-sudo apt install -y flatpak snap kitty git nala
-sudo apt install -y build-essential python3 python3-pip npm 
+sudo apt install -y xfce4
+sudo apt install -y vim emacs nano 
+sudo apt install -y flatpak snap 
+sudo apt install -y kitty git nala nix
+sudo apt install -y build-essential 
+sudo apt install -y python3 python3-pip npm 
 sudo apt install -y gcc clang cargo rustup 
-sudo apt install -y golang nodejs cowsay cmatrix 
+sudo apt install -y golang nodejs 
+sudo apt install -y cowsay cmatrix 
 sudo apt install -y cava openssl curl wget wine
-sudo apt install -y cmake default-jre default-jdk lua5.4 
-sudo apt install -y zsh rpi-imager kiwix ruby nmap gimp gpa gnupg
-sudo apt install -y yubikey-manager cryptsetup nftables tor torsocks
-sudo apt install -y docker podman qemu aircrack-ng metasploit masscan
+sudo apt install -y cmake default-jre 
+sudo apt install -y default-jdk lua5.4 
+sudo apt install -y zsh ruby nmap gpa gnupg
+sudo apt install -y yubikey-manager 
+sudo apt install -y cryptsetup nftables
+sudo apt install -y docker podman qemu 
+sudo apt install -y aircrack-ng metasploit masscan
 clear
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+mkdir -p ~/venvs/venv
+sudo python3 -m venv ~/venvs/venv
+source ~/venvs/venv/bin/activate
 sudo pip install colorama requests rich typer pipenv virtualenv psutil 
 sudo pip install paramiko fabric schedule docker cryptography pyopenssl 
 sudo pip install bcrypt scapy pwntools black flake8 pytest ipython 
-sudo pip install juypter fastapi httpie tqdm
+sudo pip install juypter fastapi httpie tqdm orjson
 clear
 
 
@@ -81,7 +86,7 @@ clear
 
 echo "Customizing.."
 mkdir -p ~/.config/fastfetch
-cp etherstartum-logo.txt ~/.config/fastfetch/logo.txt
+cp etherstratum-logo.txt ~/.config/fastfetch/logo.txt
 
 
 
@@ -92,10 +97,10 @@ echo "Setting up EtherStratum identity files..."
 
 sudo tee "$TARGET/etc/os-release" > /dev/null <<'EOF'
 NAME="EtherStratum"
-PRETTY_NAME="EtherStratum 1.1.0 (Debian-based)"
-VERSION="1.1.0"
-VERSION_ID="1.1.0"
-VERSION_CODENAME="kirbensis"
+PRETTY_NAME="EtherStratum 1.2.0 (Debian-based)"
+VERSION="1.2.0"
+VERSION_ID="1.2.0"
+VERSION_CODENAME="Danio"
 ID=etherstratum
 ID_LIKE=debian
 HOME_URL="https://github.com/MOHAPY24/EtherStratum"
@@ -108,33 +113,33 @@ EOF
 
 sudo tee "$TARGET/usr/lib/os-release" > /dev/null <<'EOF'
 NAME="EtherStratum"
-PRETTY_NAME="EtherStratum 1.1.0 (Debian-based)"
-VERSION="1.1.0"
-VERSION_ID="1.1.0"
-VERSION_CODENAME="kirbensis"
+PRETTY_NAME="EtherStratum 1.2.0 (Debian-based)"
+VERSION="1.2.0"
+VERSION_ID="1.2.0"
+VERSION_CODENAME="Danio"
 ID=etherstratum
 ID_LIKE=debian
 EOF
 
 sudo tee "$TARGET/etc/lsb-release" > /dev/null <<'EOF'
 DISTRIB_ID=EtherStratum
-DISTRIB_RELEASE=1.1.0
-DISTRIB_CODENAME=kirbensis
-DISTRIB_DESCRIPTION="EtherStratum 1.1.0 (Debian-based)"
+DISTRIB_RELEASE=1.2.0
+DISTRIB_CODENAME=Danio
+DISTRIB_DESCRIPTION="EtherStratum 1.2.0 (Debian-based)"
 EOF
 
 sudo tee "$TARGET/etc/issue" > /dev/null <<'EOF'
-EtherStratum GNU/Linux 1.1.0 (Kirbensis)
+EtherStratum GNU/Linux 1.2.0 (Kirbensis)
 Welcome to EtherStratum — run 'etherstratum' for quick info.
 EOF
 
 sudo tee "$TARGET/etc/issue.net" > /dev/null <<'EOF'
-EtherStratum GNU/Linux 1.1.0 (Kirbensis) - Authorized users only.
+EtherStratum GNU/Linux 1.2.0 (Danio) - Authorized users only.
 EOF
 
 sudo tee "$TARGET/etc/motd" > /dev/null <<'EOF'
-Welcome to EtherStratum Kirbensis (1.1.0)
-Run: etherstratum --help
+Welcome to EtherStratum Kirbensis (1.2.0)
+Run: etherstratum 
 EOF
 
 
@@ -157,8 +162,8 @@ echo "Verify with: cat $TARGET/etc/os-release, lsb_release -a, and login to see 
 
 
 echo "creating sandbox and temp directories.."
-sudo mkdir -p /vat /temp /var
-sudo chmod 777 /vat /temp
+sudo mkdir -p /vat /tmp /var
+sudo chmod 777 /vat 
 
 echo "Setting up TestVat System..."
 
@@ -190,7 +195,6 @@ clear
 fastfetch --logo ~/.config/fastfetch/logo.txt
 alias python="python3"
 alias ffs="fastfetch"
-alias nef="neofetch"
 alias vsc="codium"
 alias lsa="ls -a"
 alias py="python3"
@@ -203,7 +207,11 @@ alias print="echo"
 alias etherstratum="echo EtherStratum v1.1.0"
 alias firf="firefox"
 alias brvb="brave-browser"
+alias fastfetch="fastfetch --logo ~/.config/fastfetch/logo.txt"
 alias clearvat="sudo rm -rf /vat/*"
+alias cleartmp="sudo rm -rm /tmp/*"
+
+source ~/venvs/venv/bin/activate
 ALIASES
 
 echo "Finalizing.."
